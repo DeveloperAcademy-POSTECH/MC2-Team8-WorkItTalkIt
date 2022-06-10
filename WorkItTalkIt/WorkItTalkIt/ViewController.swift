26f5c7103588da7a4a075547ae92c174c10210ec
skycat0212 <mrmagnet777@gmail.com>
2022-06-10 11:23:18 +0900

    🎉 [INIT] init project

---
+++ WorkItTalkIt/WorkItTalkIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkItTalkIt
+//
+//  Created by 김수진 on 2022/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorkItTalkIt/WorkItTalkItUITests/WorkItTalkItUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WorkItTalkItUITestsLaunchTests.swift
+//  WorkItTalkItUITests
+//
+//  Created by 김수진 on 2022/06/10.
+//
+
+import XCTest
+
+class WorkItTalkItUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
