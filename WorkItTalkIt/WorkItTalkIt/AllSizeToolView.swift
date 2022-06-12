29d057844119f8a2f073c5787a7c46c006c17836
GODNOEL <103012104+GODNOEL@users.noreply.github.com>
2022-06-12 13:15:22 +0900

    ✅ [ADD] #5 사이즈별 툴 그리기 (small, medium)

---
+++ WorkItTalkIt/WorkItTalkIt/AllSizeToolView.swift
@@ -0,0 +1,15 @@
+//
+//  AllSizeToolView.swift
+//  WorkItTalkIt
+//
+//  Created by Yu ahyeon on 2022/06/11.
+//
+
+import UIKit
+
+class AllSizeToolView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ WorkItTalkIt/WorkItTalkIt/AllToolSizeView.swift
@@ -0,0 +1,16 @@
+//
+//  AllToolSizeView.swift
+//  WorkItTalkIt
+//
+//  Created by Yu ahyeon on 2022/06/11.
+//
+
+import UIKit
+
+class AllToolSizeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
