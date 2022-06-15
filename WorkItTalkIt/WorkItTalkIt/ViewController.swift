1fd6991f23ec34dcb07a16ecd89ab9cf906ded11
HANS <103012796+HANS5109@users.noreply.github.com>
2022-06-15 16:58:45 +0900

    ✅ [ADD] 메인화면, 편집화면 UI구성 및 Long Press Gesture 구현 #17 #18

--- WorkItTalkIt/WorkItTalkIt/ViewController.swift
+++ WorkItTalkIt/WorkItTalkIt/ViewController.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    @IBOutlet var gestureArea: UIImageView!
+    @IBAction func didTabEditButton(_ sender: UIButton) {
+        if let editScreen = self.storyboard?.instantiateViewController(withIdentifier: "EditScreenVC") {
+            editScreen.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+            self.present(editScreen, animated: true)
+            print(type(of: self))
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        gestureArea.backgroundColor = .clear
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(longPressed))
+        longPressGesture.minimumPressDuration = 0.8
+        longPressGesture.delegate = self
+        self.view.addGestureRecognizer(longPressGesture)
     }
-
+    @objc func longPressed(gestureRecognizer: UILongPressGestureRecognizer) {
+        if gestureRecognizer.state != UIGestureRecognizer.State.ended {
+            gestureArea.backgroundColor = .systemGray
+        } else {
+            gestureArea.backgroundColor = .cyan
+        }
+        print("Long Pressed")
+    }
 }
