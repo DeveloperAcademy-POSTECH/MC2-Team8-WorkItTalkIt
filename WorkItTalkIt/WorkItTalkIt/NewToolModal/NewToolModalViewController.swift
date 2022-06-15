08caa000fe8bb9cfe5f305f76b195c8475d9f21e
Lena <99120199+lenamin@users.noreply.github.com>
2022-06-15 22:42:11 +0900

    ✅[ADD] #3 코드리뷰 반영 - 변경된 enum에 따라 NewToolModalViewController 수정

--- WorkItTalkIt/WorkItTalkIt/NewToolModal/NewToolModalViewController.swift
+++ WorkItTalkIt/WorkItTalkIt/NewToolModal/NewToolModalViewController.swift
@@ -8,75 +8,78 @@
 import UIKit
 
 class NewToolModalViewController: UIViewController {
-    
-    // expandable
-    @IBOutlet weak var categoryTableView: UITableView! = {
+
+    var toolCategory: [ToolCategory] = ToolCategory.allCases
+
+    @IBOutlet var categoryTableView: UITableView! = {
         let categoryTableView = UITableView()
         categoryTableView.register(UITableViewCell.self, forCellReuseIdentifier: "NewToolCategoryTableViewCell")
         return categoryTableView
     }()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(categoryTableView)
-        
+
         categoryTableView.delegate = self
         categoryTableView.dataSource = self
+
+        view.addSubview(categoryTableView)
     }
 }
 
 extension NewToolModalViewController: UITableViewDelegate, UITableViewDataSource {
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
-        return categories.count
+        return toolCategory.count
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if categories[section].isExpanded {
-            return categories[section].subCategories.count + 1
+        if toolCategory[section].hasArrow {
+            return (toolCategory[section].subCategory?.count ?? 0) + 1
         } else {
             return 1
         }
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = categoryTableView.dequeueReusableCell(withIdentifier: "NewToolCategoryTableViewCell", for: indexPath) as? NewToolCategoryTableViewCell else { return UITableViewCell()}
-        
+
+        guard let cell = categoryTableView.dequeueReusableCell(withIdentifier: "NewToolCategoryTableViewCell", for: indexPath) as? NewToolCategoryTableViewCell else { return UITableViewCell() }
+
         // 카테고리 표시
         if indexPath.row == 0 {
-            cell.categoryTitleLabel.text = categories[indexPath.section].categoryTitle
-            cell.categoryImage.image = categories[indexPath.section].categoryImage
-            cell.categoryArrowImage.isHidden = !categories[indexPath.section].isArrow
+            cell.categoryTitleLabel.text = toolCategory[indexPath.section].title
+            cell.categoryImage.image = toolCategory[indexPath.section].image
+            cell.categoryArrowImage.isHidden = !toolCategory[indexPath.section].hasArrow
             // ArrowImage 구분하기
-            if !categories[indexPath.section].isArrow {
-                cell.categoryArrowImage.isHidden = true
+            if !toolCategory[indexPath.section].hasArrow {
+                cell.categoryArrowImage.image = nil
             }
-            
-            if categories[indexPath.section].isExpanded {
-                cell.categoryArrowImage.isHighlighted = categories[indexPath.section].isExpanded
+            if toolCategory[indexPath.section].isExpanded {
+                cell.categoryArrowImage.isHighlighted = toolCategory[indexPath.section].isExpanded
             }
-        
+
         // 펼쳐져 있으면 카테고리 내 sub 카테고리를 표시할 조건문
         } else {
-            cell.categoryArrowImage.isHidden = true
-            cell.categoryTitleLabel.text = categories[indexPath.section].subCategories[indexPath.row - 1].categoryTitle
-            cell.categoryImage.image = categories[indexPath.section].subCategories[indexPath.row - 1].categoryImage
+            cell.categoryArrowImage.image = nil
+            cell.categoryTitleLabel.text = toolCategory[indexPath.section].subCategory?[indexPath.row - 1].title
+            cell.categoryImage.image = toolCategory[indexPath.section].subCategory?[indexPath.row - 1].image
         }
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
         categoryTableView.deselectRow(at: indexPath, animated: true)
-        
+
         if indexPath.row == 0 {
             // 카테고리를 접었다 편다
-            categories[indexPath.section].isExpanded = !categories[indexPath.section].isExpanded
-            
+            // print("1\(toolCategory[indexPath.section].isExpanded)")
+            toolCategory[indexPath.section].isExpanded = !toolCategory[indexPath.section].isExpanded
+            // print("2\(toolCategory[indexPath.section].isExpanded)")
+
             // 테이블뷰 reload
-            categoryTableView.reloadSections([indexPath.section], with: .automatic)
-        }
-        else {
+            categoryTableView.reloadSections([indexPath.section], with: .none)
+        } else {
             print("tapped sub categories")
         }
     }
